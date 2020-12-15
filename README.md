d939c4bed7f3f43fe2b28257b9ab9833db57104f
Mine Producer <mine.producer@merico.dev>
2020-12-14 16:43:40 -0800

    First commit (adding 5 mines)

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## Criteria for minesweeper
+
+When a function's cyclomatic complexity goes from above or equals to 10 to below 10.
+
+## Test Description
+
+This repo tests if we can correctly compute a developer's minesweeper grade if they have multiple emails. Here's the setup:
+
+- Developer A has three emails: a1@merico.dev, a2@merico.dev, and a3@merico.dev
+- Developer B has one email: b@merico.dev 
+- Each of developer A's emails swept 1 mine
+- Developer B's only email swept 2 mines
+
+So if we merge emails correctly, developer A would be ranked No.1 for minesweeper badge.
---
+++ main.py
@@ -0,0 +1,103 @@
+
+
+ def test1():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
+
+
+def test2():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
+
+
+def test3():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
+
+def test4():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
+
+def test5():
+    if a:
+        return a
+    elif b:
+        return b
+    elif c:
+        return c
+    elif d:
+        return d
+    elif e:
+        return e
+    elif f:
+        return f
+    elif g:
+        return g
+    elif h:
+        return h
+    elif i:
+        return i
